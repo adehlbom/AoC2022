8eba4f9a696c88a5c6f079246120e3cd1e0a930b
Anders Dehlbom <8577496+adehlbom@users.noreply.github.com>
2022-12-01 21:20:12 +0100

    Day 1 finished

---
+++ Day_1/day_1.py
@@ -0,0 +1,31 @@
+
+#splitlines-funktion --> lista
+#funktion som printar ut maxnummer based on max i listan
+
+
+import fileinput
+
+#hantera file input med stödfunktion
+def file_input(addr):
+    return addr.readlines()
+    
+def group_numbers(list):
+    lists = [[]]
+    for i in list:
+        if i == "\n":
+            lists.append([])
+        else:
+            lists[-1].append(i)
+    return [[item.strip() for item in group] for group in lists]
+
+def sum_list(list):
+    temp_list = list
+    temp_list = [[int(item) for item in group] for group in temp_list]
+    return [sum(a) for a in temp_list]
+#Part 1
+with open('input.txt') as f:
+    #print(max(sum_list(group_numbers(file_input(f)))))
+
+#Part 2
+    sorted_list = sorted(sum_list(group_numbers(file_input(f))))
+    print(sum(sorted_list[-3:]))
